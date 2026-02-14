cba18f405f9a8a6c994b49e7d0b6eeb3f6943286
lczhuigz <125116125+LCzhuigz@users.noreply.github.com>
2026-02-14 21:55:56 +0800

    2026-2-14

---
+++ C/exercise/11.c
@@ -0,0 +1,35 @@
+/*
+题目：  
+    C语言实现打印一个菱形图案
+*/
+#include <stdio.h>
+
+
+#define line 7
+
+
+int main(int argc, char ** argv){
+    int i;
+    for(i = 0; i < line; i ++){
+        for(int space = 0; space < line - i; space ++){
+            printf(" ");
+        }
+        for(int k = 0; k < i * 2 + 1; k ++){
+            printf("*");
+        }
+        putchar('\n');
+    }
+
+    for(i = line - 1; i >= 1; i --){
+        for(int space = 0; space < line - i; space ++){
+            printf(" ");
+        }
+        for(int k = 1; k < i * 2 + 1; k ++){
+            printf("*");
+        }
+        putchar('\n');
+    }
+
+    
+    return 0;
+}
